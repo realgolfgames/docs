786faffefc437b2ac318c5fe6704145828f7317a
MoinJulian <jh@realgolf.games>
2025-06-06 17:25:41 +0100

    feat: add German and English documentation for RealGolf.Games API and types

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,40 @@
 // @ts-check
-import starlight from '@astrojs/starlight';
-import { defineConfig } from 'astro/config';
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/developer/docs/',
+  base: "/developer/docs/",
   integrations: [
     starlight({
-      title: 'RealGolf.Games Developer Docs',
-      favicon: 'logo.PNG',
+      title: {
+        en: "RealGolf.Games Developer Documentation",
+        de: "RealGolf.Games Entwicklerdokumentation",
+      },
+      defaultLocale: "en",
+      locales: {
+        en: {
+          label: "English",
+          lang: "en",
+        },
+        de: {
+          label: "Deutsch",
+        },
+      },
+      favicon: "logo.PNG",
+      social: [{ icon: "github", label: "GitHub", href: "https://github.com/realgolfgames/docs" }],
       sidebar: [
         {
-          label: 'Types',
+          label: "Types",
           items: [
-            { label: 'Introduction', link: '/types/introduction' },
-            { label: 'Blog', link: '/types/blog' },
-            { label: 'Course', link: '/types/course' },
-            { label: 'Game', link: '/types/game' },
-            { label: 'Rule', link: '/types/rule' }
-          ]
-        }
-      ]
-    })
-  ]
+            { label: "Introduction", link: "/types/introduction" },
+            { label: "Blog", link: "/types/blog" },
+            { label: "Course", link: "/types/course" },
+            { label: "Game", link: "/types/game" },
+            { label: "Rule", link: "/types/rule" },
+          ],
+        },
+      ],
+    }),
+  ],
 });
